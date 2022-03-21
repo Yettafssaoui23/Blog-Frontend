bf27d9041234a0cf8182537cdb13b2612304bd2e
Yettafssaoui23 <yettafssaoui2@gmail.com>
2022-03-21 21:37:36 +0100

    Trying to signup with mongoose

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,39 +1,61 @@
 import React, { useState } from "react";
 import { Col, Container, Row, Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import './Signup.css'; 
+import "./Signup.css";
+import axios from "axios";
 
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  // e means an event 
-  function handleLogin(e) {
+  // e means an event
+  function handleSignup(e) {
     e.preventDefault();
+    axios
+      .post("http://localhost:5000/users", { email, password })
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
   }
   return (
     <Container className="signup-cont">
       <Row>
-      <Col md={5} className="signup__bg--container"></Col>
-        <Col md={7} className="d-flex align-items-center justify-content-center">
-          <Form className="signup__form" onSubmit={handleLogin}>
+        <Col md={5} className="signup__bg--container"></Col>
+        <Col
+          md={7}
+          className="d-flex align-items-center justify-content-center"
+        >
+          <Form className="signup__form" onSubmit={handleSignup}>
             <h1>Create Your Account</h1>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </Form.Group>
             <Button variant="dark" type="submit">
               Login to your Account
             </Button>
             <div className="py-4">
-                <p>Already, have an account ?  You can Login in <Link to="/login">Here</Link> </p>            </div>
+              <p>
+                Already, have an account ? You can Login in{" "}
+                <Link to="/login">Here</Link>{" "}
+              </p>{" "}
+            </div>
           </Form>
         </Col>
       </Row>
